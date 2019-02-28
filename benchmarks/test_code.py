e5cff5dd4b92499e154b1d8bccc1b2548439ce87
MichielLangen <michiel.langen@kpnmail.nl>
2019-02-28 11:25:45 +0100

    feat(benchmark): trying to add machine learning code to use as a benchmark

---
+++ benchmarks/test_code.py
@@ -0,0 +1,35 @@
+import sys
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.datasets import load_iris 
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from observatory.tracking import TrackingSession, start_run, LocalState, RemoteState
+import pdb
+
+# load the iris dataset as an example 
+
+iris = load_iris()   
+# store the feature matrix (X) and response vector (y) 
+X = iris.data 
+y = iris.target 
+  
+# splitting X and y into training and testing sets 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1) 
+pdb.set_trace()
+with observatory.start_run("testmodel", 1) as run:
+# training the model on training set 
+    knn = KNeighborsClassifier(n_neighbors=3) 
+    knn.fit(X_train, y_train)
+    run.record_metric 
+  
+# making predictions on the testing set 
+y_pred = knn.predict(X_test) 
+# comparing actual response values (y_test) with predicted response values (y_pred) 
+print("kNN model accuracy:", metrics.accuracy_score(y_test, y_pred)) 
+  
+# making prediction for out of sample data 
+sample = [[3, 5, 4, 2], [2, 3, 5, 4]] 
+preds = knn.predict(sample) 
+pred_species = [iris.target_names[p] for p in preds] 
+print("Predictions:", pred_species) 
+  
