562df9308e495ca693697ce93d4a7660104ad194
MichielLangen <michiel.langen@kpnmail.nl>
2019-04-04 13:58:24 +0200

    chore: added unit-tests for Archive module

--- tests/test_Archive.py
+++ tests/test_Archive.py
@@ -4,27 +4,36 @@ from os.path import expanduser
 
 import pytest
 import requests
+from observatory.archive import Archive
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def test_file():
-    path = expanduser('~') + "\\.observatory\\metrics\\test_v1_test_12345678-255.pkl"
+    os.remove(expanduser('~' + '\\.observatory\\metrics\\test_v1_test_test.pkl'))
+    path = expanduser('~') + "\\.observatory\\metrics\\test_v1_test_12345678-017f-41ce-b4b7-735bf7123332.pkl"
     data = []
-    for x in range(100):
-        data.append(['loss', x])
+    data.append(['loss', 255])
     with open(path, 'wb') as f:
         pickle.dump(data, f)
     yield path
 
-    os.remove(expanduser('~') + "\\.observatory\\metrics\\test_v1_test_12345678-255.pkl")
+    os.remove(expanduser('~') + "\\.observatory\\metrics\\test_v1_test_12345678-017f-41ce-b4b7-735bf7123332.pkl")
 
-def test_get_model():
-    pass
+def test_get_model(test_file):
+    data = Archive.get_model(Archive, 'test', test_file[:-53])
 
-def test_get_version():
-    pass
+    assert data == ['1']
 
-def test_get_experiment():
-    pass
+def test_get_version(test_file):
+    data = Archive.get_version(Archive, 'test', '1', test_file[:-53])
 
-def test_get_run():
-    pass
+    assert data == ['test']
+
+def test_get_experiment(test_file):
+    data = Archive.get_experiment(Archive, 'test', '1', 'test', test_file[:-53])
+
+    assert data == ['12345678']
+
+def test_get_run(test_file):
+    data = Archive.get_run('12345678', test_file[:-53])
+
+    assert data == [[['loss', 255]]]
